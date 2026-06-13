6e441c13b04f3397e894598df976651946153f91
luis <luis.g.mendonca@aluno.senai.br>
2026-06-13 13:51:21 -0300

    atualização do program

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,19 +1,33 @@
+using Microsoft.OpenApi;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls(
-    $"http://*:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}"
+    $"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}"
 );
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi();
+
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        document.Servers.Clear();
+
+        document.Servers.Add(new OpenApiServer
+        {
+            Url = "https://aulaapionrender.onrender.com"
+        });
+
+        return Task.CompletedTask;
+    });
+});
 
 var app = builder.Build();
 
 app.MapControllers();
-
 app.MapOpenApi();
 
 app.MapScalarApiReference(options =>
