f31ec3add1a461270cc9d796c2b424029ac0c113
luis <luis.g.mendonca@aluno.senai.br>
2026-06-13 13:21:18 -0300

    Ajustando o program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,7 +3,7 @@ using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls(
-    $"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}"
+    $"http://* :{Environment.GetEnvironmentVariable("PORT") ?? "8080"}"
 );
 
 builder.Services.AddControllers();
